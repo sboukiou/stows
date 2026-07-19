fd1d2040662d0c63822d414d7f413ce6669003d4
Said <sboukiou@student.1337.ma>
2026-07-19 23:47:17 +0000

    Restored old config

---
+++ config/nvim/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,23 @@
+return {
+	{
+		"toppair/peek.nvim",
+		event = { "VeryLazy" },
+		build = "deno task --quiet build:fast",
+		config = function()
+			local peek = require('peek')
+
+			vim.api.nvim_create_user_command('PeekOpen', function()
+					if not peek.is_open() and vim.bo[vim.api.nvim_get_current_buf()].filetype == 'markdown' then
+					vim.fn.system('i3-msg split horizontal')
+					peek.open()
+					end
+					end, {})
+
+			vim.api.nvim_create_user_command('PeekClose', function()
+					if peek.is_open() then
+					peek.close()
+					vim.fn.system('i3-msg move left')
+					end
+					end
+					},
+}
